a5477f58cf4cbd0e2fd4ffb0d4ecdfe71d80f89f
Ross Moug <ross.moug@solarwinds.com>
2020-02-19 14:17:28 +0000

    Additional project setup

---
+++ src/schema.ts
@@ -0,0 +1,6 @@
+import { GraphQLModule } from "@graphql-modules/core";
+import { myFirstModule } from "./modules/my-first-module";
+
+export const { schema } = new GraphQLModule({
+  imports: [],
+});
